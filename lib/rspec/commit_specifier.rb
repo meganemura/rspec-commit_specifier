ea08241db3af8c20301959d37aafc27f38f5d3d2
meganemura <meganemura@users.noreply.github.com>
2015-05-29 03:37:00 +0900

    Implement #commits

--- lib/rspec/commit_specifier.rb
+++ lib/rspec/commit_specifier.rb
@@ -1,7 +1,30 @@
 require "rspec/commit_specifier/version"
+require "rugged"
 
 module RSpec
   module CommitSpecifier
-    # Your code goes here...
+    def repository
+      Rugged::Repository.discover(current_directory)
+    end
+
+    def current_directory
+      Dir.pwd
+    end
+
+    def commits
+      walker.walk
+    end
+
+    def walker
+      walker = Rugged::Walker.new(repository)
+      walker.sorting(Rugged::SORT_DATE | Rugged::SORT_REVERSE)
+      walker.push('HEAD')
+      walker.hide(master_object_id)
+      walker
+    end
+
+    def master_object_id
+      repository.ref('refs/remotes/origin/master').target_id
+    end
   end
 end
--- spec/rspec/commit_specifier_spec.rb
+++ spec/rspec/commit_specifier_spec.rb
@@ -1,11 +1,29 @@
 require 'spec_helper'
 
 describe RSpec::CommitSpecifier do
+  include described_class
+
   it 'has a version number' do
     expect(RSpec::CommitSpecifier::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  describe 'commits' do
+    subject { commits }
+
+    it 'returns commits' do
+      expect(commits.count).to be >= 0
+      expect(commits.map(&:message)).to all(be)
+    end
+
+    describe 'messages' do
+      subject { super().map(&:message) }
+
+      it { is_expected.to all(be) }
+      it { is_expected.to all(match(/\A.*(?:\n\Z|\n\n)/)) }   # force second line to be blank
+      it { is_expected.to all(match(/\A.*[^.]\n/)) }          # force first line to end with [^.]
+
+      commands = %w(bundle rake rails guard sed)
+      it { is_expected.to all(match(/\A(?:[A-Z0-9]|#{commands.join('|')})/)) }  # start with uppercase or command
+    end
   end
 end
